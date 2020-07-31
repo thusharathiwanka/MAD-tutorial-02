4677af6cbe373912b8a767722c47bad97f1ea131
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-08-01 02:24:38 +0530

    first string added

--- app/src/main/java/com/example/usingstrings/ActivityOne.java
+++ app/src/main/java/com/example/usingstrings/ActivityOne.java
@@ -1,14 +1,17 @@
 package com.example.usingstrings;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityOne extends AppCompatActivity {
+//    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_one);
+
+//        textView2 = findViewById(R.id.textView);
     }
 }
