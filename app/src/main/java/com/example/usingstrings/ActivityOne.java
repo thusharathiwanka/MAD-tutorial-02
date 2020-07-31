328f51cd1371a091cf2a477cd6538f29a85b953b
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-08-01 02:27:05 +0530

    second text view added

--- app/src/main/java/com/example/usingstrings/ActivityOne.java
+++ app/src/main/java/com/example/usingstrings/ActivityOne.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class ActivityOne extends AppCompatActivity {
-//    TextView textView2;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
 
-//        textView2 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView2.setText(R.string.Msg2);
     }
 }
